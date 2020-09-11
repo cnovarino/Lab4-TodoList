887a1a838ebfca1817668bebe71adb2d560d326d
unknown <novacrez@gmail.com>
2020-09-11 16:54:53 -0300

    API integrated

--- src/App.svelte
+++ src/App.svelte
@@ -4,25 +4,74 @@
 
 <script>
 	import { Col, Container, Row, Card, CardHeader, CardTitle, CardText, CardBody, CardFooter, Input, Form, FormGroup, Label, ListGroup, ListGroupItem } from "sveltestrap";
+	import {onMount} from "svelte";
 	let counter = 0;
 	let items = [];
 	let completedItems = [];
 
 	let todoInput;
 
-	function addTodo(event) {
+	async function addTodo(event) {
 		event.preventDefault();
-		if(todoInput.length === 0){
+		if (todoInput.length === 0) {
 			alert("Debe ingresar una tarea");
 			return;
 		}
-		items = [...items, { id: ++counter, text: todoInput, completed: false}];
+
+		const formData = new FormData();
+		formData.append('task',todoInput);
+
+		await fetch(`http://127.0.0.1:5001/api/todos`, {
+			method: 'POST',
+			body: formData
+		})
+		.then(data => data.json())
+		.then(response => {
+			items = response;
+			//Todo check errors
+		});
+
 		todoInput = "";
 	}
 
-	function deleteTodo(todo){
-		items = items.filter(function (item){ return item.id !== todo.id});
+	async function deleteTodo(todo) {
+
+		await fetch(`http://127.0.0.1:5001/api/todos/` + todo[0], {
+			method: 'DELETE'
+		})
+				.then(data => data.json())
+				.then(response => {
+					items = response;
+					//Todo check errors
+				});
 	}
+
+	async function updateTodo(event,todo) {
+
+		const formData = new FormData();
+		formData.append('completed',event.target.checked ? 1 : 0);
+
+		await fetch(`http://127.0.0.1:5001/api/todos/` + todo[0], {
+			method: 'PUT',
+			body: formData
+		})
+				.then(data => data.json())
+				.then(response => {
+					console.log(response);
+					//Todo check errors
+				});
+	}
+
+
+	onMount(async () => {
+		await fetch(`http://127.0.0.1:5001/api/todos`,{
+		})
+				.then(data => data.json())
+				.then(response => {
+					items = response;
+					//Todo check errors
+				});
+	})
 
 </script>
 
@@ -59,9 +108,14 @@
 							{#each items as item}
 								<ListGroupItem>
 									<Row>
-										<Col xs="9">{item.text}</Col>
+										<Col xs="9">{item[1]}</Col>
 										<Col xs="2">
-											<input bind:group={completedItems} value="{item.id}" type="checkbox" />
+											<input
+													on:change={event => updateTodo(event,item)}
+													value="{item[0]}"
+													type="checkbox"
+													bind:checked={item[2]}
+											/>
 										</Col>
 										<Col>
 											<a on:click={_ => deleteTodo(item)} class="delete_todo" href="#">X</a>
